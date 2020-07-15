a8121ab20d7c4a784058f6ff940c6a072de132f5
karyRmartinez <karymartinez@pursuit.org>
2020-07-15 15:05:11 -0400

    background images- design

---
+++ dice-and-Cards/dice-and-Cards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dice-and-Cards
+//
+//  Created by Kary Martinez on 7/15/20.
+//  Copyright © 2020 Kary Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dice-and-Cards/dice-and-CardsTests/dice_and_CardsTests.swift
@@ -0,0 +1,34 @@
+//
+//  dice_and_CardsTests.swift
+//  dice-and-CardsTests
+//
+//  Created by Kary Martinez on 7/15/20.
+//  Copyright © 2020 Kary Martinez. All rights reserved.
+//
+
+import XCTest
+@testable import dice_and_Cards
+
+class dice_and_CardsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
